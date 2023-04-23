ed1c505213e1c3d32469ca8f34ea4662e7e90b3d
Leonardo Caravetta <leocara825@gmail.com>
2023-04-22 22:53:18 -0300

    Preentrega2+Caravetta

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
+import { productos } from "./data/stock";
 import { ItemCount } from "./components/ItemCount/ItemCount";
+import {
+    BrowserRouter as Router, 
+    Routes,
+    Navigate,
+    Route,
+} from "react-router-dom";
 
 function App(){
     return(
-        <div className="App">
-            <main>
-                <NavBar/>
-                <ItemListContainer saludo = "Bienvenido!"/>
-                <ItemCount/>
-            </main>
-        </div>
+        <><div>
+            
+        </div><div className="App">
+                <Router>
+                    <NavBar />
+                    <ItemCount />
+                    <Routes>
+                        <Route path="/" element={<ItemListContainer />} />
+                        <Route path="/counter" element={<ItemCount />} />
+                        <Route path="*" element={<Navigate />} />
+                    </Routes>
+                </Router>
+            </div></>
     );
 }
 
