38db36996a46c55fca19d15db0d204cb77a0e7dc
Leonardo Caravetta <leocara825@gmail.com>
2023-04-29 14:49:16 -0300

    Preentrega2+Caravetta

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { productos } from '../../data/stock'
 import { Item } from '../Item/Item'
 
-export const ItemList = () => {
+export const ItemList = ({productos=[]}) => {
   return (
     <div>
         <h3>Productos</h3>
